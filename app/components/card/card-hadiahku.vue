96ba5c913c4867acf720b04e019b8daa0e63ea04
nizarfazari <nexsel32@gmail.com>
2026-02-08 20:39:11 +0700

    fix: card promo and hadiah

--- app/components/card/card-hadiahku.vue
+++ app/components/card/card-hadiahku.vue
@@ -1,17 +1,17 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
- <div class="border overflow-hidden bg-background-secondary border-[#FFFFFF26] rounded-[10px]">
-  <div class="">
-   <img class="w-full" src="/images/hadiahku/hadiahku.png" alt="">
+  <div
+    class="border overflow-hidden bg-background-secondary border-[#FFFFFF26] rounded-[10px] hover:border-secondary cursor-pointer transition duration-200"
+  >
+    <div class="">
+      <img class="w-full" src="@/assets/images/hadiahku/hadiahku.png" alt="" />
+    </div>
+    <div class="text-white px-5 py-4">
+      <p class="font-open-sans text-xs font-normal mb-2">1 januari 2026</p>
+      <h3 class="font-montserrat text-[14px] font-semibold">Hadiahku</h3>
+    </div>
   </div>
-  <div class="text-white px-5 py-4">
-   <p class="font-open-sans text-xs font-normal mb-2">1 januari 2026</p>
-   <h3 class="font-montserrat text-[14px] font-semibold">Hadiahku</h3>
-  </div>
- </div>
 </template>
 
 <style scoped></style>
